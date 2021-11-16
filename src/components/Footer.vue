5d066d434dc484c61146cd80d9bcb9e467345fd6
Zach Kamp <zach@zachkamp.com>
2021-11-16 12:48:19 -0500

    footer fix

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,9 +1,11 @@
 <template>
-  <ul class="footer-nav mt-5">
-    <li>Contact Us</li>
-    <li>Terms of Use</li>
-    <li>Privacy Policy</li>
-  </ul>
+  <div class="footer-nav mt-5 d-flex justify-content-center">
+    <ul class="d-flex justify-content-between">
+      <li>Contact Us</li>
+      <li>Terms of Use</li>
+      <li>Privacy Policy</li>
+    </ul>
+  </div>
 </template>
 
 <script>
@@ -13,16 +15,17 @@ export default {
 </script>
 
 <style scoped>
-ul.footer-nav {
-  list-style-type: none;
-  padding: 0.5rem 1rem;
-  display: flex;
-  width: 100%;
-  justify-content: space-between;
-  /* position: absolute;
-  bottom: 0; */
+.footer-nav {
   background-color: #fff;
   margin: initial;
+}
+
+.footer-nav ul {
+  list-style-type: none;
+  width: 600px;
+  padding: 0.5rem 1rem;
+  /* position: absolute;
+  bottom: 0; */
   font-size: 0.875rem;
   font-family: Lato, Arial, sans-serif;
   color: #454545;
