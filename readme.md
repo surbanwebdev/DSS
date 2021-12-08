dfa6a7a1c248fb482e1862f55279f0139d8e4dfc
Allen Sizemore <asizemore@daxor.com>
2021-12-08 19:16:55 +0000

    Preserving work

--- readme.md
+++ readme.md
@@ -1,1 +1,2 @@
 docker volume create database
+docker save dss_client:latest > dss_client.tar && docker save dss_server:latest dss_server.tar
--- server/src/Logic/db.js
+++ server/src/Logic/db.js
@@ -3,24 +3,24 @@ const sqlite = require('sqlite');
 const fs = require('fs');
 
 const dbPath = '/usr/src/app/db/DecisionSupport.db';
+
 const dbParams = {
     filename: dbPath,
     driver: sqlite3.Database
 }
 
-async function getDB(){
-    if (!fs.existsSync(dbPath)){
-        console.log("Hey dude it works!")
+async function getDB() {
+    if (!fs.existsSync(dbPath)) {
         await setupDB();
     }
     return await sqlite.open(dbParams);
 }
 
-function setupDB(){
-    return new Promise(async (resolve,reject)=>{
-        try{
-            const db = await sqlite.open(dbParams);
-            const archivedTreatment = `
+async function setupDB() {
+    try {
+
+        const db = await sqlite.open(dbParams);
+        const archivedTreatment = `
                 CREATE TABLE "ArchivedTreatment" (
                 "TestId"	INTEGER NOT NULL UNIQUE,
                 "PatientId"	INTEGER NOT NULL,
@@ -34,14 +34,14 @@ function setupDB(){
                 "SuggestedTreatment"	TEXT NOT NULL,
                 PRIMARY KEY("TestId" AUTOINCREMENT)
             );`;
-            const patient = `CREATE TABLE Patient (
+        const patient = `CREATE TABLE Patient (
                 PatientID TEXT NOT NULL PRIMARY KEY UNIQUE,
                 Sex TEXT NOT NULL,
                 Weight REAL NOT NULL,
                 Height REAL NOT NULL
             );`;
 
-            const user = `CREATE TABLE "User" (
+        const user = `CREATE TABLE "User" (
                 "ID"	INTEGER NOT NULL UNIQUE,
                 "FirstName"	TEXT NOT NULL,
                 "LastName"	TEXT NOT NULL,
@@ -51,7 +51,7 @@ function setupDB(){
                 "IsAdmin"	TEXT,
                 PRIMARY KEY("ID" AUTOINCREMENT)
             );`;
-            const userSession = `CREATE TABLE "UserSession" (
+        const userSession = `CREATE TABLE "UserSession" (
                 "UserSessionID"	INTEGER NOT NULL UNIQUE,
                 "UserID"	INTEGER NOT NULL,
                 "UserSessionGUID"	TEXT NOT NULL UNIQUE,
@@ -62,21 +62,20 @@ function setupDB(){
                 "UserPassword"	TEXT NOT NULL,
                 PRIMARY KEY("UserSessionID" AUTOINCREMENT)
             );`;
-            const createUser = `INSERT INTO User (FirstName,LastName,UserName,UserPassword,Language,IsAdmin) 
+        const createUser = `INSERT INTO User (FirstName,LastName,UserName,UserPassword,Language,IsAdmin) 
             VALUES('Admin','User','admin','admin','English','Yes');`;
 
-            await db.run(user);
-            await db.run(createUser);
-            await db.run(userSession);
-            await db.run(patient);
-            await db.run(archivedTreatment);
-            resolve();
+        await db.run(user);
+        await db.run(createUser);
+        await db.run(userSession);
+        await db.run(patient);
+        await db.run(archivedTreatment);
+        return;
 
-        }catch(e){
-            console.error(e);
-            reject(e);
-        }
-    });
+    } catch (e) {
+        console.error(e);
+        throw (e);
+    }
 }
 
 module.exports = {
